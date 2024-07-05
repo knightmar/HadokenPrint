50409545744a65d726ac36eec3040bb26f4da5d9
knightmar <arthur.petit.d@gmail.com>
2024-07-05 14:20:19 +0200

    improve code + added closed loop system on the arduino part (python part on the next commit)

--- arduino/motor/motor.ino
+++ arduino/motor/motor.ino
@@ -1,26 +1,50 @@
 #include <UstepperS32.h>
 
+// Create a new instance of the uStepper class
 UstepperS32 stepper;
-#define POSITION_REACHED 0x20
 
 void setup() {
-  stepper.setup();                   //Initialize uStepper S32
-  stepper.checkOrientation(30.0);    //Check orientation of motor connector with +/- 30 microsteps movement
-  stepper.setMaxAcceleration(5000);  //use an acceleration of 2000 fullsteps/s^2
-  stepper.setMaxVelocity(1000);       //Max velocity of 500 fullsteps/s
-  Serial.begin(115200);
+  // Initialize the serial communication
+  Serial.begin(9600);
+
+  // Setup the stepper motor
+  stepper.setup();
+
+  // Set the maximum velocity and acceleration
+  stepper.setMaxAcceleration(5000);
+  stepper.setMaxVelocity(1000);
+  stepper.checkOrientation(30.0);
 }
 
 void loop() {
-
+  // Check if there is new data in the serial buffer
   if (Serial.available() > 0) {
-    int angle = Serial.parseInt();
-    stepper.moveAngle(angle);
+    // Read the input from the serial buffer
+    String input = Serial.readStringUntil('\n');
 
-    while (!stepper.getMotorState(POSITION_REACHED)) {
-      delay(10);
+    // Check if the input is "HOME"
+    if (input == "HOME") {
+      // Set the home position of the stepper motor
+      stepper.encoder.setHome();
+      Serial.println("Home position set");
+    } else {
+      // Try to parse the input as an angle
+      int angle = input.toInt();
+
+      // Check if the parsing was successful
+      if (angle != 0) {
+        // Move the stepper motor to the given angle
+        stepper.moveToAngle(angle);
+
+        if (abs(angle - stepper.angleMoved()) > 0.5) {
+          Serial.println("Bad move");
+          stepper.moveToAngle(angle);
+        }
+        Serial.println("OK");
+      } else {
+        // The input was not "HOME" and not a valid angle
+        Serial.println("Invalid input");
+      }
     }
-
-    Serial.println("ok");
   }
 }
